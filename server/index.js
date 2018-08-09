d96b3c1021a90c62a74c657296eca6a9633cb970
elliotBraem <braemelliot@gmail.com>
2018-08-09 15:03:31 -0500

    debug added

--- server/index.js
+++ server/index.js
@@ -3,12 +3,11 @@ var path = require('path');
 var compression = require('compression');
 var helmet = require('helmet');
 var morgan = require('morgan');
-// var debug = require('debug')('app')
+var debug = require('debug')('app')
 var app = express();
 
 // const isDev = (process.env.NODE_ENV || 'development') === 'development'
 
-// try {
 // if (isDev) {
 //   const webpack = require('webpack')
 //   const webpackConfig = require('../webpack.config')
@@ -21,28 +20,29 @@ var app = express();
 // }
 
 // not sure if this is necessary
-process.on('uncaughtException', function(er) {
-  // debug('Error: %o', er.stack)
-  console.error(er.stack);
-  process.exit(1);
-});
+try {
+  process.on('uncaughtException', function(er) {
+    debug('Error: %o', er.stack)
+    console.error(er.stack);
+    process.exit(1);
+  });
 
-app.use(morgan('dev'));
-app.use(compression());
-app.use(helmet());
-app.use(function(err, req, res) {
-  console.error(err.stack);
-  res.status(500).send('Error caught!');
-});
+  app.use(morgan('dev'));
+  app.use(compression());
+  app.use(helmet());
+  app.use(function(err, req, res) {
+    console.error(err.stack);
+    res.status(500).send('Error caught!');
+  });
 
-app.set('view engine', 'pug');
-app.set('views', path.resolve(__dirname, 'views'));
+  app.set('view engine', 'pug');
+  app.set('views', path.resolve(__dirname, 'views'));
 
-app.get('/', (req, res) => {
-  res.render('index');
-});
+  app.get('/', (req, res) => {
+    res.render('index');
+  });
 
-app.listen(process.env.PORT || 8080, () => console.log('Server running!'));
-// } catch(err) {
-//   debug('Error: %o', err)
-// }
+  app.listen(process.env.PORT || 8080, () => console.log('Server running!'));
+} catch(err) {
+  debug('Error: %o', err)
+}
